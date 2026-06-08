cac63acfe2e5a7749838ebea766bbe8b4c9fc346
Dave <david.tomusk@gmail.com>
2026-06-08 21:34:04 +0100

    Set up router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
-function App() {
+import { RouterProvider } from "react-router-dom"
+import { router } from "./app/AppRoutes"
 
+function App() {
   return (
-    <>
-    hi
-    </>
+    <RouterProvider router={router} />
   )
 }
 
