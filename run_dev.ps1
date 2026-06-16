dc3e4a85a2eb87d2a0858acc442b745b90b7e854
Dave <david.tomusk@gmail.com>
2026-06-16 10:36:40 +0100

    Move web and app around

--- run_dev.ps1
+++ run_dev.ps1
@@ -1,3 +1,3 @@
-Start-Process cmd -ArgumentList '/k', 'cd frontend && pnpm run dev'
+Start-Process cmd -ArgumentList '/k', 'cd apps && cd web && pnpm run dev'
 
 Start-Process cmd -ArgumentList '/k', 'cd server && cargo watch -x "run --bin server"'
